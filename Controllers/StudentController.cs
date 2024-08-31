35d48d5f0c615f1b26391e035959ada0a07318e4
RaneemDaqa <raneemdaqa9@gmail.com>
2024-08-31 18:59:54 +0300

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        public ActionResult GetAll()
+        {
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult Update()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
